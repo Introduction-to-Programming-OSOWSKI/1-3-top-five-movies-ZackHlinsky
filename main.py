118b380216ddb1af542fba9828aef37f2646514b
Hlinsky Zachary <819460@stma.k12.mn.us>
2021-03-25 10:27:21 -0500

    topFiveMovies

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a, b, c, d, e,):
-    return a + b + c + d + e
+    return a  +  b  +  c  +  d  +  e
 
 print(topFiveMovies("Star Wars ", "Back to the Future ", "Cars ", "Iron man ", "Shrek "))
